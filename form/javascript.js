cceaaafb1730e4f8f1c84460783bc50fa47cf1e7
daande01 <daniel.evert.andersson@gmail.com>
2019-11-26 11:11:05 +0100

    new

--- form/javascript.js
+++ form/javascript.js
@@ -8,9 +8,13 @@ function printuser(){
 
 
   var namn =document.getElementById("us").value;
-    var färg =document.getElementById("färg").value;
+  var färg =document.getElementById("färg").value;
 
+  var gender=document.getElementById("kön").value;
 
-  document.getElementById("par").innerHTML= namn+"färg "+färg;
+    document.body.style.backgroundColor=färg;
+    document.getElementById("par").style.fontSize = "100px";
+
+  document.getElementById("par").innerHTML= namn+"färg "+färg+ " "+gender;
 
 }

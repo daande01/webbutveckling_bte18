aced02cba4ebbfc4271fc7b54cb98167dc570d65
daande01 <daniel.evert.andersson@gmail.com>
2019-12-03 10:26:30 +0100

    Update javascript.js
    new

--- form/javascript.js
+++ form/javascript.js
@@ -1,20 +1,18 @@
 
 document.getElementById("knapp").addEventListener("click", printuser);
 
-
+document.body.style.backgroundColor = localStorage.getItem("färg");
 
 
 function printuser(){
 
 
-  var namn =document.getElementById("us").value;
+
   var färg =document.getElementById("färg").value;
+  localStorage.setItem("färg", färg);
+  document.body.style.backgroundColor=färg;
 
-  var gender=document.getElementById("kön").value;
 
-    document.body.style.backgroundColor=färg;
-    document.getElementById("par").style.fontSize = "100px";
 
-  document.getElementById("par").innerHTML= namn+"färg "+färg+ " "+gender;
 
 }

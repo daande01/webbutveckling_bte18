78631fa2f44506ff06b69fb41e860f4ca48d6a61
daande01 <daniel.evert.andersson@gmail.com>
2020-01-15 08:52:34 +0100

    test

--- javascript/slider/js/slider.js
+++ javascript/slider/js/slider.js
@@ -1,6 +1,6 @@
 
 
- $("#window").css("overflow", "hidden"); // om javascript är av kommer denna rad inte att köras , då blir alla bilder synliga 
+ $("#window").css("overflow", "hidden"); // om javascript är "av" kommer denna rad inte att köras , då blir alla bilder synliga 
  // jag har även ändrat i css en gällande overflow. detta kommer att leda till att bilderna blinkar till och syns
  // en kort stund innan javascriptet ser till att de får hidden. så i detta fall kan denna javascript kod behöva placeras 
  // tidigare i html dokumnetet exempelvis i header så att de läses in innan man ritat ut html objekten på skärmen.
